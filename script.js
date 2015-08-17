dd618df54b81473c68a81cf1d5478072ebc3180e
Claudette Ngai <claudettengai@Claudettes-Air.hsd1.ca.comcast.net>
2015-08-17 00:26:55 -0700

    rough mockup of alert box on right panel -- having problems with fixed panel at the bottom of the page

--- script.js
+++ script.js
@@ -15,6 +15,49 @@ $(document).ready(function(){
 		};
 
 
+		$.fn.scrollBottom = function() { 
+ 		 return $(document).height() - this.scrollTop() - this.height(); 
+		};
+
+		var fromBottom= $(window).scrollBottom();
+		// console.log(fromBottom);
+		var stop_scroll_bottom = $(document).height() - $(window).height() + 140; 
+		console.log(stop_scroll_bottom);
+
+
+
+
+
+
+
+
+		if(fromBottom<=140){
+			$('.right-rail').css({
+				position:'absolute',
+				top: stop_scroll_bottom,
+			});
+		};
+
+
+
+
+
+		// Ask Chris -- how do I remove the above statement when fromBottom>140? I want the 
+		//scroll to act the way that it was acting before that statement.  
+
+
+
+
+
+
+		// if(fromBottom>140){
+		// 	$('.right-rail').css({
+		// 		position: 'fixed',
+
+		// 	});
+		// };
+
+
 	});
 
 });
